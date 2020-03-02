7e1b6cf27382be5066ed0d5f946ac0f44113d100
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2020-03-02 10:31:29 +0000

    International address forms were using wrong country list. This has been amended.

--- app/controllers/leadtrustee/organisation/NonUkAddressController.scala
+++ app/controllers/leadtrustee/organisation/NonUkAddressController.scala
@@ -26,7 +26,7 @@ import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import repositories.PlaybackRepository
 import uk.gov.hmrc.play.bootstrap.controller.FrontendBaseController
-import utils.countryOptions.CountryOptions
+import utils.countryOptions.CountryOptionsNonUK
 import views.html.leadtrustee.organisation.NonUkAddressView
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -40,7 +40,7 @@ class NonUkAddressController @Inject()(
                                       formProvider: NonUkAddressFormProvider,
                                       val controllerComponents: MessagesControllerComponents,
                                       view: NonUkAddressView,
-                                      countryOptions: CountryOptions
+                                      countryOptions: CountryOptionsNonUK
                                      )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
 
   val form = formProvider()
