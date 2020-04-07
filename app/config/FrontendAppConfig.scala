fe13580f412938c6fa8a4202c94e9e6ec1ab7164
Adam Conder <adamdconder@gmail.com>
2020-04-07 13:28:22 +0100

    Aligning deskpro service name

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -26,7 +26,7 @@ import play.api.mvc.Call
 class FrontendAppConfig @Inject() (configuration: Configuration) {
 
   private val contactHost = configuration.get[String]("contact-frontend.host")
-  private val contactFormServiceIdentifier = "maintain-lead-trustee-frontend"
+  private val contactFormServiceIdentifier = "trusts"
 
   def claimATrustUrl(utr: String) =
     configuration.get[Service]("microservice.services.claim-a-trust-frontend").baseUrl + s"/claim-a-trust/save/$utr"
