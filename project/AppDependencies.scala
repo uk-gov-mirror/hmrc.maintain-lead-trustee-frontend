53116b49ed1b13e7463f55688063eb111676bf80
Jay Nabonne <jaynabonne@gmail.com>
2020-05-13 09:13:53 +0100

    Updated some dependencies.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,23 +3,23 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
-  val compile = Seq(
+  val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "org.reactivemongo" %% "play2-reactivemongo"            % "0.18.8-play26",
-    "uk.gov.hmrc"       %% "logback-json-logger"            % "3.1.0",
-    "uk.gov.hmrc"       %% "govuk-template"                 % "5.36.0-play-26",
-    "uk.gov.hmrc"       %% "play-health"                    % "3.14.0-play-26",
-    "uk.gov.hmrc"       %% "play-ui"                        % "8.2.0-play-26",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "0.2.0",
-    "uk.gov.hmrc"       %% "bootstrap-play-26"              % "1.1.0",
-    "uk.gov.hmrc"       %% "domain"                         % "5.6.0-play-26",
+    "uk.gov.hmrc"       %% "logback-json-logger"            % "4.8.0",
+    "uk.gov.hmrc"       %% "govuk-template"                 % "5.54.0-play-26",
+    "uk.gov.hmrc"       %% "play-health"                    % "3.15.0-play-26",
+    "uk.gov.hmrc"       %% "play-ui"                        % "8.9.0-play-26",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.2.0-play-26",
+    "uk.gov.hmrc"       %% "bootstrap-play-26"              % "1.7.0",
+    "uk.gov.hmrc"       %% "domain"                         % "5.8.0-play-26",
     "com.typesafe.play" %% "play-json-joda"                 % "2.7.4",
-    "uk.gov.hmrc"       %% "play-whitelist-filter"          % "2.0.0",
+    "uk.gov.hmrc"       %% "play-whitelist-filter"          % "3.3.0-play-26",
     "org.typelevel"     %% "cats-core"                      % "2.0.0",
     "javax.inject"      %  "javax.inject"                   % "1"
   )
 
-  val test = Seq(
+  val test: Seq[ModuleID] = Seq(
     "org.scalatest"               %% "scalatest"              % "3.0.7",
     "org.scalatestplus.play"      %% "scalatestplus-play"     % "3.1.2",
     "org.pegdown"                 %  "pegdown"                % "1.6.0",
