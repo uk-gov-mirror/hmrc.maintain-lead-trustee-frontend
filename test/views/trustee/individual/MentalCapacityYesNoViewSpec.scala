1c971f1182ed0b1a9932bc0cda53846a520b9d3c
carrielies <carrielies@users.noreply.github.com>
2021-03-10 11:22:09 +0000

    TRUS-3821 Add CountryOfNationality and MentalCapacity Pages

--- test/views/trustee/individual/MentalCapacityYesNoViewSpec.scala
+++ test/views/trustee/individual/MentalCapacityYesNoViewSpec.scala
@@ -17,32 +17,35 @@
 package views.trustee.individual
 
 import forms.YesNoFormProvider
+import models.{Name, NormalMode}
 import play.api.data.Form
 import play.twirl.api.HtmlFormat
 import views.behaviours.YesNoViewBehaviours
-import views.html.trustee.individual.CountryOfResidenceInTheUkYesNoView
 import controllers.trustee.individual.routes
-import models.NormalMode
-import controllers.trustee.individual.routes
+import views.html.trustee.individual.MentalCapacityYesNoView
 
-class CountryOfResidenceInTheUkYesNoViewSpec extends YesNoViewBehaviours {
+class MentalCapacityYesNoViewSpec extends YesNoViewBehaviours {
 
-  val messageKeyPrefix = "trustee.individual.countryOfResidenceInTheUkYesNo"
-  val name = "Name"
+  val messageKeyPrefix = "trustee.individual.mentalCapacityYesNo"
+  val name: Name = Name("First", None, "Last")
 
-  val form = new YesNoFormProvider().withPrefix(messageKeyPrefix)
+  val form: Form[Boolean] = new YesNoFormProvider().withPrefix(messageKeyPrefix)
 
-  "CountryOfResidenceInTheUkYesNoView view" must {
+  "MentalCapacityYesNoView" must {
 
-    val view = viewFor[CountryOfResidenceInTheUkYesNoView](Some(emptyUserAnswers))
+    val view = viewFor[MentalCapacityYesNoView](Some(emptyUserAnswers))
 
     def applyView(form: Form[_]): HtmlFormat.Appendable =
-      view.apply(form, NormalMode, name)(fakeRequest, messages)
+      view.apply(form, NormalMode, name.displayName)(fakeRequest, messages)
 
-    behave like dynamicTitlePage(applyView(form), messageKeyPrefix, name)
+    behave like dynamicTitlePage(applyView(form), messageKeyPrefix, name.displayName)
 
     behave like pageWithBackLink(applyView(form))
 
-    behave like yesNoPage(form, applyView, messageKeyPrefix, Some(name), routes.CountryOfResidenceInTheUkYesNoController.onSubmit(NormalMode).url)
+    behave like yesNoPage(form, applyView, messageKeyPrefix, Some(name.displayName), routes.MentalCapacityYesNoController.onSubmit(NormalMode).url)
+
+    behave like pageWithHint(form, applyView, s"$messageKeyPrefix.hint")
+
+    behave like pageWithASubmitButton(applyView(form))
   }
 }
