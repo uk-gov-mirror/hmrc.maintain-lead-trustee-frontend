7b706523d1e05d10c2f8142a029c21bf642b1f30
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2021-03-15 09:48:19 +0000

    Controllers and views for lead trustee organisation.

---
+++ test/views/leadtrustee/organisation/CountryOfResidenceViewSpec.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.leadtrustee.organisation
+
+import forms.CountryFormProvider
+import models.NormalMode
+import play.api.data.Form
+import play.twirl.api.HtmlFormat
+import utils.InputOption
+import utils.countryOptions.CountryOptionsNonUK
+import views.behaviours.SelectCountryViewBehaviours
+import views.html.leadtrustee.organisation.CountryOfResidenceView
+
+class CountryOfResidenceViewSpec extends SelectCountryViewBehaviours {
+
+  val prefix = "leadtrustee.organisation.countryOfResidence"
+  val name = "Name"
+
+  val form: Form[String] = new CountryFormProvider().withPrefix(prefix)
+
+  "CountryOfResidence view" must {
+
+    val view = viewFor[CountryOfResidenceView](Some(emptyUserAnswers))
+
+    val countryOptions: Seq[InputOption] = app.injector.instanceOf[CountryOptionsNonUK].options
+
+    def applyView(form: Form[_]): HtmlFormat.Appendable =
+      view.apply(form, NormalMode, countryOptions, name)(fakeRequest, messages)
+
+    behave like dynamicTitlePage(applyView(form), prefix, name)
+
+    behave like pageWithBackLink(applyView(form))
+
+    behave like selectCountryPage(form, applyView, prefix, name)
+
+    behave like pageWithASubmitButton(applyView(form))
+  }
+}
